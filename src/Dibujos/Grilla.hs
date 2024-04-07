b72fa611086318b95cd0566fc98011997a2e18c5
RenzoSC <renzoc009@gmail.com>
2024-04-07 20:17:55 -0300

    Dibujo grilla done

--- src/Dibujos/Grilla.hs
+++ src/Dibujos/Grilla.hs
@@ -9,11 +9,12 @@ import Dibujo (Dibujo, juntar, apilar, figura)
 import FloatingPic (Conf(..), Output)
 
 import Graphics.Gloss ( text, scale, translate )
+import GHC.Float (int2Float)
 
-type Coord = (Float, Float)
+type Coord = (Int, Int)
 
 interpCoord :: Output Coord
-interpCoord coord _ _ _= uncurry translate coord $ scale 0.1 0.1 $ text (show coord)
+interpCoord (x,y) _ _ _= translate (int2Float (x*100 + 35)) (int2Float ((7-y)*100 +45)) $ scale 0.1 0.1 $ text (show (x,y))
 
 row :: [Dibujo a] -> Dibujo a
 row [] = error "row: no puede ser vacío"
@@ -29,7 +30,7 @@ grilla :: [[Dibujo a]] -> Dibujo a
 grilla = column . map row
 
 paintNums :: Dibujo Coord
-paintNums = grilla [[figura (x, y) | y <- [0..7]] | x <- [0..7]]
+paintNums = grilla [[ figura (x, y) | y <- [0..7]] | x <- [7,6,5,4,3,2,1,0]]
 
 grillaConf :: Conf
 grillaConf = Conf {
